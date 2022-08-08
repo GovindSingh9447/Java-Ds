a37fce736bf1ed48f3eaa4d81525be8a03e8b9b9
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-08-08 09:31:47 +0530

    .

--- Rest Api/rest_anime_boot/src/main/java/com/restapi/anime/Services/AnimeServices.java
+++ Rest Api/rest_anime_boot/src/main/java/com/restapi/anime/Services/AnimeServices.java
@@ -5,9 +5,17 @@ import com.restapi.model.Anime;
 
 public class AnimeServices { 
 
-    private List<Anime> list =new ArrayList<>();
+    private static List<Anime> list =new ArrayList<>();
     static{
-       list.add(new Anime(11))
+       list.add(new Anime(11,"Attack On Titan","Heluchash",233,"OnGoing","War"));
+       list.add(new Anime(12,"Attack On Titan","Heluchash",243,"OnGoing","War"));
+       list.add(new Anime(13,"Attack On Titan","Heluchash",253,"OnGoing","War"));
+       list.add(new Anime(14,"Attack On Titan","Heluchash",263,"OnGoing","War"));
+    }
+
+    //get All Animes
+    public List<Anime> getAllAnimes(){
+        return list;
     }
     
 }
